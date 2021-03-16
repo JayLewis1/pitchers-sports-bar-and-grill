f12d078e8fad09d44f6d16d595add79179f19fb4
Jay Lewis <jr_lewis@outlook.com>
2021-03-16 14:00:46 +0000

    Changes on styling and functionality on responsive

---
+++ web/build/static/js/8.89c8365d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[8],{44:function(n,t,c){"use strict";c.r(t);var u=c(0),e=c(3);t.default=Object(e.g)((function(n){var t=n.history;return Object(u.useEffect)((function(){var n=t.listen((function(){window.scrollTo(0,0)}));return function(){n()}}),[t]),null}))}}]);
+//# sourceMappingURL=8.89c8365d.chunk.js.map
